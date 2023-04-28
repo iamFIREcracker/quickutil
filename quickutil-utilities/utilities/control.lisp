4d1b50046f84b7a00a9758a7e272af0ac8b27d32
Matteo Landi <matteo@matteolandi.net>
2023-04-28 15:25:12 +0200

    Add Clojure's RECURSIVELY

--- quickutil-utilities/utilities/control.lisp
+++ quickutil-utilities/utilities/control.lisp
@@ -21,3 +21,21 @@
     `(until (not ,expression)
        ,@body))
   %%%)
+
+(defutil recursively (:version (1 . 0)
+                      :depends-on let1
+                      :category (language misc))
+  "Execute `body` recursively, like Clojure's `loop`/`recur`.
+
+`bindings` should contain a list of symbols and (optional) starting values.
+
+In `body` the symbol `recur` will be bound to the function for recurring."
+  #>%%%>
+  (defmacro recursively (bindings &body body)
+    (let ((names (mapcar #'(lambda (b) (if (atom b) b (first b))) bindings))
+          (values (mapcar #'(lambda (b) (if (atom b) nil (second b))) bindings)))
+      (let1 recur (intern "RECUR")
+        `(labels ((,recur (,@names)
+                    ,@body))
+           (,recur ,@values)))))
+  %%%)
