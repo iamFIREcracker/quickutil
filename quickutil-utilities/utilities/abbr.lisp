0cff73bdc7c2c43ab3b554097b072ca33ba6c3b2
Matteo Landi <matteo@matteolandi.net>
2023-12-04 16:08:51 +0100

    Add FLET* to alias LABELS

--- quickutil-utilities/utilities/abbr.lisp
+++ quickutil-utilities/utilities/abbr.lisp
@@ -1,5 +1,15 @@
 (in-package #:quickutil-utilities.utilities)
 
+(defutil flet* (:version (1 . 0)
+                :category (language abbr))
+  "Like LABELS, but 1 character shorter.
+Also, FLET* is to FLET what LET* is to LET."
+  #>%%%>
+  (defmacro flet* (&rest body)
+    %%DOC
+    `(labels ,@body))
+  %%%)
+
 (defutil if-not (:version (1 . 0)
                  :category control)
   "Like IF, except TEST gets wrapped inside NOT."
