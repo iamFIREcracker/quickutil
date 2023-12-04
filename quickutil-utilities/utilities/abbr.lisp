66cb4cc94ae2a1894788d2d63643171cdf5e50a7
Matteo Landi <matteo@matteolandi.net>
2023-12-04 16:11:58 +0100

    Add ABBR utility, and use it to generate a bunch of abbreviations

--- quickutil-utilities/utilities/abbr.lisp
+++ quickutil-utilities/utilities/abbr.lisp
@@ -3,7 +3,9 @@
 (defutil flet* (:version (1 . 0)
                 :category (language abbr))
   "Like LABELS, but 1 character shorter.
-Also, FLET* is to FLET what LET* is to LET."
+Also, FLET* is to FLET what LET* is to LET.
+
+Note: cannot use ABBR for this, because LABELS is a special operator."
   #>%%%>
   (defmacro flet* (&rest body)
     %%DOC
@@ -18,3 +20,42 @@ Also, FLET* is to FLET what LET* is to LET."
     %%DOC
     `(if (not ,test) ,then ,else))
   %%%)
+
+(defutil abbr (:version (1 . 0)
+               :category (language abbr))
+  "Defines a new function/macro named `short` and sharing
+FDEFINITION/MACRO-FUNCTION with `long`."
+  #>%%%>
+  (defmacro abbr (short long)
+    %%DOC
+    `(eval-when (:compile-toplevel :load-toplevel :execute)
+       (cond
+         ((macro-function ',long)
+          (setf (macro-function ',short) (macro-function ',long))
+          #+ccl (setf (ccl:arglist ',short) (ccl:arglist ',long)))
+         ((fboundp ',long)
+          (setf (fdefinition ',short) (fdefinition ',long))
+          #+ccl (setf (ccl:arglist ',short) (ccl:arglist ',long)))
+         (t
+           (error "Can't abbreviate ~a" ',long)))))
+  %%%)
+
+(defmacro defabbrutil (short long &optional deps)
+  (let ((doc (format nil "Like ~A, but few chars shorter." (string-upcase long)))
+        (depends-on (cons 'abbr deps))
+        (body (format nil "  ~((abbr ~a ~a)~)" short long)))
+    `(defutil ,short (:version (1 . 0)
+                      :depends-on ,depends-on
+                      :category (language abbr))
+       ,doc
+       ,body)))
+
+(defabbrutil d-b destructuring-bind)
+(defabbrutil m-v-b multiple-value-bind)
+
+(defabbrutil split split-sequence (split-sequence))
+(defabbrutil split-if split-sequence-if (split-sequence))
+(defabbrutil split-if-not split-sequence-if-not (split-sequence))
+
+(defabbrutil keep-if remove-if-not)
+(defabbrutil keep-if-not remove-if)
