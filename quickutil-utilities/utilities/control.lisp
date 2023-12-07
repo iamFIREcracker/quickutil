e98653f73277cf0e5f96fe91bb259b750a869277
Matteo Landi <matteo@matteolandi.net>
2023-12-07 20:46:27 +0100

    Expand WHILE to (LOOP WHILE DO ...)
    This way its users don't have to depend on UNTIL.

--- quickutil-utilities/utilities/control.lisp
+++ quickutil-utilities/utilities/control.lisp
@@ -46,13 +46,12 @@ lexical environmnet."
   %%%)
 
 (defutil while (:version (1 . 0)
-                :depends-on until
                 :category (language control))
   "Executes `body` while `expression` is true."
   #>%%%>
   (defmacro while (expression &body body)
     %%DOC
-    `(until (not ,expression)
+    `(loop while ,expression do
        ,@body))
   %%%)
 
