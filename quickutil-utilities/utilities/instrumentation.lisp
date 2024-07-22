e161c48f115740abbf99bafb154183908264b260
Matteo Landi <matteo@matteolandi.net>
2024-07-22 17:58:56 +0200

    Debugging stuff
    - PMX to pretty-print a macro-expanded form
    - DBG to print a bunch of args, separated by spaces, and followed by
      a final newline
    - DBGL to print a bunch of args, labeled, separated by newlines, and
      followed by a final newline

--- quickutil-utilities/utilities/instrumentation.lisp
+++ quickutil-utilities/utilities/instrumentation.lisp
@@ -16,3 +16,40 @@ returns the result as the second value."
                           internal-time-units-per-second)))
          (values ,tm ,res))))
   %%%)
+
+(defutil pmx (:version (1 . 0)
+              :category instrumentation)
+  "MACROEXPAND-1 and then PRETTY-PRINT `form`."
+  #>%%%>
+  (defmacro pmx (form)
+    %%DOC
+    `(pprint (macroexpand-1 ',form)))
+  %%%)
+
+
+(defutil dbg (:version (1 . 0)
+              :category printing)
+  "Print `args` to screen, separated by a space, and followed by a newline.
+Returns the first arg."
+  #>%%%>
+  (defun dbg (&rest args)
+    %%DOC
+    (format t "~{~A~^ ~}" args)
+    (terpri)
+    (finish-output)
+    (first args))
+  %%%)
+
+(defutil dbgl (:version (1 . 0)
+               :depends-on (dbg)
+               :category printing)
+  "Print `args`, labeled, separated by a newline, and followed by a final
+newline.  Returns the last arg. labeled and readably."
+  #>%%%>
+  (defmacro dbgl (&rest args)
+    %%DOC
+    `(prog1
+       (progn ,@(mapcar (lambda (arg) `(dbg ',arg ,arg)) args))
+       (terpri)
+       (finish-output)))
+  %%%)
